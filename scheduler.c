b259ddc1de1e68a25483fe034857f891685254ed
Phuoc Hieu <hieuphamminh976@gmail.com>
2024-09-02 10:33:13 +0700

    Update scheduler.c

--- scheduler.c
+++ scheduler.c
@@ -9,73 +9,33 @@ typedef struct
     int arrival_time;
     int cpu_time;
     int resource_time;
+    int remaining_time;
+    int completion_time;
+    int waiting_time;
+    int turnaround_time;
 } Process;
 
 // function read process from the input file
-int readProcesses(char* filename, Process** process){
-    FILE* file = fopen(filename, "r");
-    if(!file){
-        perror("Error");
-        return -1;
+Process* readProcesses(char *filename, int *n){
+    FILE *file = fopen(filename, "r");
+    if(file == NULL){
+        printf("Error %s\n", filename);
+        return NULL;
     };
-    
-    int num_process;
-    fscanf(file, "%d", &num_process);
-    *process = (Process*) malloc(num_process * sizeof(Process));
+    fscanf(file, "%d", n);
+    Process *processes = (Process *) malloc((*n) * sizeof(Process));
 
-    for (int i = 0; i < num_process; i++){
-        fscanf(file, "%d %d %d", &((*process)[i].arrival_time), &((*process)[i].cpu_time), &((*process)[i].resource_time));
-        (*process)[i].process_id = i + 1; // start from1
+    for (int i = 0; i < *n; i++){
+        processes[i].process_id = i + 1; // start from 1
+        fscanf(file, "%d %d %d", &processes[i].arrival_time, &processes[i].cpu_time, &processes[i].resource_time);
+        processes[i].remaining_time = processes[i].cpu_time;
     }
 
     fclose(file);
-    return num_process;
+    return processes;
 };
 
-// function to simulate fcfs scheduling and create output file
-void fcfsScheduling(Process* processes, int num_process, char* output_filename){
-    FILE* file = fopen(output_filename, "w");
-    if(!file){
-        perror("Error");
-        return;
-    };
-
-    // output the gantt chart for cpu scheduling
-    for(int i = 0; i < num_process; i++){
-        for(int j = 0; j < processes[i].cpu_time; j++){
-            fprintf(file, "%d", processes[i].process_id);
-        }
-    }
-    fprintf(file, "\n");
-
-    fclose(file);
-}
-
-int main(int argc, char* argv[]){
-    if(argc < 4){
-        printf("Usage: %s <input file> <output file> <algorithm>\n", argv[0]);
-        return 1;
-    }
-
-    char* input_filename = argv[1];
-    char* output_filename = argv[2];
-    int algorithm = atoi(argv[3]);
-
-    Process* processes = NULL;
-    int num_processes = readProcesses(input_filename, &processes);
-
-    if(num_processes < 0){
-        return 1;
-    }
-
-    // only fcfs
-    if (algorithm == 1){
-        fcfsScheduling(processes, num_processes, output_filename);
-    } else{
-        printf("Algorithm not implemented.\n");
-        free(processes);
-        return 1;
-    }
-    free(processes);
-    return 0;
-}
+// fcfs
+void fcfsScheduling(Process* processes, int n, FILE *cpu_output, FILE *resource_output);
+// rr
+void rrScheduling(Process* processes, int n, int quantum, FILE *cpu_output, FILE *resource_output);
