a97f3dee525c8ad26d7c29486387f6c190c07219
vina123baov <vothaibao50@gmail.com>
2024-09-09 19:58:33 +0700

    pull

--- fcfs.c
+++ fcfs.c
@@ -8,27 +8,69 @@ typedef struct {
     int resource_time;
 } Process;
 
-void fcfsScheduling(Process* processes, int n, FILE *cpu_output, FILE *resource_output){
+void fcfsScheduling(Process* processes, int n){
     int current_time = 0;
+
+    printf("\nCPU Execution Timeline:\n");
     for(int i = 0; i < n; i++){
         Process *p = &processes[i];
+
+        // If the CPU is idle until the process arrives
         if(current_time < p->arrival_time){
-            // cpu idle
             for(int t = current_time; t < p->arrival_time; t++){
-                fprintf(cpu_output, "_ ");
+                printf("_ "); // Represents idle time
             }
             current_time = p->arrival_time;
         }
-        // cpu execution
-        for(int t = 0; t< p->cpu_time; t++){
-            fprintf(cpu_output, "%d ", p->process_id);
+
+        // CPU execution of the current process
+        for(int t = 0; t < p->cpu_time; t++){
+            printf("%d ", p->process_id); // Print process ID during CPU execution
             current_time++;
         }
-        // resource execution
+    }
+    printf("\n");
+
+    printf("\nResource Execution Timeline:\n");
+    for(int i = 0; i < n; i++){
+        Process *p = &processes[i];
+
+        // Resource execution of the current process
         for(int t = 0; t < p->resource_time; t++){
-            fprintf(resource_output, "%d ", p->process_id);
+            printf("%d ", p->process_id); // Print process ID during resource execution
         }
     }
-    fprintf(cpu_output, "\n");
-    fprintf(resource_output, "\n");
+    printf("\n");
+}
+
+int main(){
+    int n;
+    
+    // Get the number of processes
+    printf("Enter the number of processes: ");
+    scanf("%d", &n);
+
+    // Dynamically allocate memory for 'n' processes
+    Process *processes = (Process*)malloc(n * sizeof(Process));
+
+    // Input process details
+    for(int i = 0; i < n; i++){
+        printf("Enter details for Process %d:\n", i + 1);
+        printf("Process ID: ");
+        scanf("%d", &processes[i].process_id);
+        printf("Arrival Time: ");
+        scanf("%d", &processes[i].arrival_time);
+        printf("CPU Time: ");
+        scanf("%d", &processes[i].cpu_time);
+        printf("Resource Time: ");
+        scanf("%d", &processes[i].resource_time);
+    }
+
+    // Call FCFS scheduling function
+    fcfsScheduling(processes, n);
+
+    // Free dynamically allocated memory
+    free(processes);
+
+    return 0;
 }
--- main.c
+++ main.c
@@ -1,8 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
 
-// process structure
+// Process structure
 typedef struct 
 {
     int process_id;
@@ -15,80 +14,139 @@ typedef struct
     int turnaround_time;
 } Process;
 
-// function read process from the input file
-Process* readProcesses(char *filename, int *n){
-    FILE *file = fopen(filename, "r");
-    if(file == NULL){
-        printf("Error %s\n", filename);
-        return NULL;
-    };
-    fscanf(file, "%d", n);
-    Process *processes = (Process *) malloc((*n) * sizeof(Process));
+// Function to read processes from user input
+Process* readProcessesFromUser(int *n){
+    printf("Enter the number of processes: ");
+    scanf("%d", n);
 
+    Process *processes = (Process *) malloc((*n) * sizeof(Process));
     for (int i = 0; i < *n; i++){
-        processes[i].process_id = i + 1; // start from 1
-        fscanf(file, "%d %d %d", &processes[i].arrival_time, &processes[i].cpu_time, &processes[i].resource_time);
+        printf("Enter details for Process %d\n", i + 1);
+        processes[i].process_id = i + 1;
+        printf("Arrival Time: ");
+        scanf("%d", &processes[i].arrival_time);
+        printf("CPU Time: ");
+        scanf("%d", &processes[i].cpu_time);
+        printf("Resource Time: ");
+        scanf("%d", &processes[i].resource_time);
         processes[i].remaining_time = processes[i].cpu_time;
     }
 
-    fclose(file);
     return processes;
-};
+}
 
 // First-Come, First-Served (FCFS)
-void fcfsScheduling(Process* processes, int n, FILE *cpu_output, FILE *resource_output);
+void fcfsScheduling(Process* processes, int n){
+    int current_time = 0;
+    printf("\nCPU Execution Timeline (FCFS):\n");
+    for(int i = 0; i < n; i++){
+        Process *p = &processes[i];
+        if(current_time < p->arrival_time){
+            for(int t = current_time; t < p->arrival_time; t++){
+                printf("_ ");  // CPU idle
+            }
+            current_time = p->arrival_time;
+        }
+        for(int t = 0; t < p->cpu_time; t++){
+            printf("%d ", p->process_id);  // CPU execution
+            current_time++;
+        }
+    }
+    printf("\n");
+
+    printf("\nResource Execution Timeline (FCFS):\n");
+    for(int i = 0; i < n; i++){
+        Process *p = &processes[i];
+        for(int t = 0; t < p->resource_time; t++){
+            printf("%d ", p->process_id);  // Resource execution
+        }
+    }
+    printf("\n");
+}
+
 // Round Robin (RR)
-void rrScheduling(Process* processes, int n, int quantum, FILE *cpu_output, FILE *resource_output);
-//Shortest Job First (SJF)
-void sjfScheduling(Process *processes, int n, FILE *cpu_output, FILE *resource_output);
-// Shortest Remaining Time Next (SRTN)
-void srtnScheduling(Process *processes, int n, FILE *cpu_output, FILE *resource_output);
+void rrScheduling(Process* processes, int n, int quantum){
+    int current_time = 0, done = 0;
+    printf("\nCPU Execution Timeline (RR, quantum = %d):\n", quantum);
 
-// main
-int main(int argc, char *argv[]){
-    if(argc < 4){
-        printf("Use: %s <input_file> <output_file> <algorithm> [quantum]\n", argv[0]);
-        return 1;
+    while(done < n){
+        done = 0;
+        for(int i = 0; i < n; i++){
+            if(processes[i].remaining_time > 0){
+                if(processes[i].arrival_time <= current_time){
+                    int exec_time = (processes[i].remaining_time < quantum) ? processes[i].remaining_time : quantum;
+                    for(int t = 0; t < exec_time; t++){
+                        printf("%d ", processes[i].process_id);  // CPU execution
+                        current_time++;
+                    }
+                    processes[i].remaining_time -= exec_time;
+                } else {
+                    printf("_ ");  // CPU idle
+                    current_time++;
+                }
+            } else {
+                done++;
+            }
+        }
     }
-    
-    char *input_file = argv[1];
-    char *output_file = argv[3];
-    int algorithm = atoi(argv[3]);
-    int quantum = 2; // default for rr
+    printf("\n");
+}
 
-    if(algorithm == 2 && argc == 5){
-        quantum = atoi(argv[4]);
+// Shortest Job First (SJF) Scheduling
+void sjfScheduling(Process *processes, int n){
+    // Implement SJF logic here
+    printf("SJF scheduling logic goes here\n");
+}
+
+// Shortest Remaining Time Next (SRTN) Scheduling
+void srtnScheduling(Process *processes, int n){
+    // Implement SRTN logic here
+    printf("SRTN scheduling logic goes here\n");
+}
+
+// Main function
+int main(){
+    int n, algorithm, quantum = 2;
+
+    // Read processes from user
+    Process *processes = readProcessesFromUser(&n);
+
+    // Select algorithm
+    printf("\nSelect Scheduling Algorithm:\n");
+    printf("1. First-Come, First-Served (FCFS)\n");
+    printf("2. Round Robin (RR)\n");
+    printf("3. Shortest Job First (SJF)\n");
+    printf("4. Shortest Remaining Time Next (SRTN)\n");
+    printf("Enter choice: ");
+    scanf("%d", &algorithm);
+
+    // If Round Robin is chosen, ask for quantum
+    if(algorithm == 2){
+        printf("Enter time quantum for Round Robin: ");
+        scanf("%d", &quantum);
     }
 
-    int n;
-    Process *processes = readProcesses(input_file, &n);
-    if(processes == NULL){
-        printf("Error reading input file\n");
-        return 1;
-    }
-
-    FILE *cpu_output = fopen(output_file, "w");
-    FILE *resource_output = fopen("resource_output.txt", "w"); // divide file resource output
-
+    // Perform the selected scheduling algorithm
     switch(algorithm){
-        case 1: 
-            fcfsScheduling(processes, n, cpu_output, resource_output);
+        case 1:
+            fcfsScheduling(processes, n);
             break;
-        case 2: 
-            rrScheduling(processes, n, quantum, cpu_output, resource_output);
-            break;    
-        case 3: 
-            sjfScheduling(processes, n, cpu_output, resource_output);
+        case 2:
+            rrScheduling(processes, n, quantum);
             break;
-        case 4: 
-            srtnScheduling(processes, n, cpu_output, resource_output);
-            break;    
+        case 3:
+            sjfScheduling(processes, n);
+            break;
+        case 4:
+            srtnScheduling(processes, n);
+            break;
         default:
-            printf("Invalid algorithm choice");
+            printf("Invalid algorithm choice.\n");
             return 1;
     }
-    fclose(cpu_output);
-    fclose(resource_output);
+
+    // Free allocated memory
     free(processes);
+
     return 0;
 }
